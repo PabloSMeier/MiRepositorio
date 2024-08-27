4a8b7bd0911787cb96a4515a1fd141218cecaeee
psmeier <psmeier05@gmail.com>
2024-08-27 12:36:34 -0300

    avances motion

--- mi-proyecto/src/components/habilidades/Habilidades.tsx
+++ mi-proyecto/src/components/habilidades/Habilidades.tsx
@@ -2,15 +2,43 @@
 "use client";
 
 import { ISkill, skillsF } from "@/helpers/skills/skills.helper";
+import { motion, useAnimation } from "framer-motion";
+import { useInView } from "react-intersection-observer";
+
+import { useEffect } from "react";
 
 const Habilidades = () => {
+  const controls = useAnimation();
+
+  // Hook para detectar cuando el elemento está en vista
+  const { ref, inView } = useInView({
+    threshold: 0.1, // El div será considerado en vista cuando un 10% sea visible
+    triggerOnce: true, // La animación solo se dispara una vez
+  });
+
+  // Efecto para controlar la animación según la visibilidad del div
+  useEffect(() => {
+    if (inView) {
+      controls.start({
+        x: 0, // Mover el div a su posición original
+        opacity: 1, // Hacer que el div sea completamente visible
+        transition: { duration: 1, ease: "easeOut" }, // Configuración de la transición
+      });
+    }
+  }, [controls, inView]);
+
   return (
     <section
       id="habilidades"
-      className="h-[100vh] w-[100vw] p-6 bg-[#eeeeee] flex flex-col space-y-10 items-center md:hidden md:absolute"
+      className="h-[100vh] w-[100vw] p-6 bg-[#eeeeee] flex flex-col space-y-10 items-center lg:hidden lg:absolute"
     >
       <h1 className="text-3xl font-bold">Habilidades</h1>
-      <div className="flex flex-col space-y-4">
+      <motion.div
+        className="flex flex-col space-y-4"
+        ref={ref}
+        initial={{ x: -250, opacity: 0 }}
+        animate={controls}
+      >
         <h2 className="text-xl font-semibold">Front End</h2>
         <div className="max-w-md flex flex-wrap justify-center gap-2 md:hidden md:absolute">
           {skillsF.map((skill: ISkill) => {
@@ -27,7 +55,7 @@ const Habilidades = () => {
             );
           })}
         </div>
-      </div>
+      </motion.div>
     </section>
   );
 };
