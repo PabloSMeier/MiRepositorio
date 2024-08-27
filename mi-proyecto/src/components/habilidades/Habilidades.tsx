3f04b3a69bfee567c22acaa32f2b7e01dfb77a96
psmeier <psmeier05@gmail.com>
2024-08-27 14:07:27 -0300

    mas avances motion

--- mi-proyecto/src/components/habilidades/Habilidades.tsx
+++ mi-proyecto/src/components/habilidades/Habilidades.tsx
@@ -2,42 +2,52 @@
 "use client";
 
 import { ISkill, skillsF } from "@/helpers/skills/skills.helper";
-import { motion, useAnimation } from "framer-motion";
-import { useInView } from "react-intersection-observer";
-
-import { useEffect } from "react";
+import { motion, useInView } from "framer-motion";
+import { useRef } from "react";
 
 const Habilidades = () => {
-  const controls = useAnimation();
-
-  // Hook para detectar cuando el elemento está en vista
-  const { ref, inView } = useInView({
-    threshold: 0.1, // El div será considerado en vista cuando un 10% sea visible
-    triggerOnce: true, // La animación solo se dispara una vez
-  });
-
-  // Efecto para controlar la animación según la visibilidad del div
-  useEffect(() => {
-    if (inView) {
-      controls.start({
-        x: 0, // Mover el div a su posición original
-        opacity: 1, // Hacer que el div sea completamente visible
-        transition: { duration: 1, ease: "easeOut" }, // Configuración de la transición
-      });
-    }
-  }, [controls, inView]);
-
+  const ref = useRef(null);
+  const ref1 = useRef(null);
+  const isInView = useInView(ref);
+  const isInView1 = useInView(ref1);
   return (
     <section
       id="habilidades"
-      className="h-[100vh] w-[100vw] p-6 bg-[#eeeeee] flex flex-col space-y-10 items-center lg:hidden lg:absolute"
+      className="min-h-[100vh] w-[100vw] p-6 bg-[#eeeeee] flex flex-col space-y-10 items-center lg:hidden lg:absolute"
     >
       <h1 className="text-3xl font-bold">Habilidades</h1>
       <motion.div
-        className="flex flex-col space-y-4"
+        className={`flex transition-all duration-1000 flex-col space-y-4 ${
+          isInView
+            ? "transform-none opacity-1"
+            : "opacity-0 translate-x-[-200px]"
+        }`}
         ref={ref}
-        initial={{ x: -250, opacity: 0 }}
-        animate={controls}
+      >
+        <h2 className="text-xl font-semibold">Front End</h2>
+        <div className="max-w-md flex flex-wrap justify-center gap-2 md:hidden md:absolute">
+          {skillsF.map((skill: ISkill) => {
+            return (
+              <div
+                key={skill.id}
+                className="group w-1/4 h-[6.5rem] flex flex-col items-center justify-center border-2 border-[#a1a1a1] shadow-lg rounded-lg overflow-hidden relative"
+              >
+                <img className="w-14" src={skill.img} alt="" />
+                <h1 className="text-xl text-black font-semibold absolute bottom-0 transform translate-y-full group-hover:translate-y-0 transition-transform ease-in-out duration-300">
+                  {skill.name}
+                </h1>
+              </div>
+            );
+          })}
+        </div>
+      </motion.div>
+      <motion.div
+        className={`flex transition-all duration-1000 flex-col space-y-4 ${
+          isInView1
+            ? "transform-none opacity-1"
+            : "opacity-0 translate-x-[200px]"
+        }`}
+        ref={ref1}
       >
         <h2 className="text-xl font-semibold">Front End</h2>
         <div className="max-w-md flex flex-wrap justify-center gap-2 md:hidden md:absolute">
