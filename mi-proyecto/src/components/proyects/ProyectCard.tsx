29c32eb2240e41e447d943d81c5cdeb6e38a3c7d
psmeier <psmeier05@gmail.com>
2024-09-24 16:41:30 -0300

    correccion svg

--- mi-proyecto/src/components/proyects/ProyectCard.tsx
+++ mi-proyecto/src/components/proyects/ProyectCard.tsx
@@ -35,11 +35,11 @@ const ProyectCard = ({ proyect }: { proyect: IProyect }) => {
                   width="26"
                   height="26"
                   viewBox="0 0 24 24"
-                  stroke-width="1.5"
+                  strokeWidth="1.5"
                   stroke="#ffffff"
                   fill="none"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
                 >
                   <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                   <path d="M3 7m0 2a2 2 0 0 1 2 -2h14a2 2 0 0 1 2 2v9a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2z" />
